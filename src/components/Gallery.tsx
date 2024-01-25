7496c38c268eaf5dd6ef70487d9d58182e0c2f25
Richard McGirt <richardmcgirt@Richards-Mac-mini.local>
2024-01-24 21:03:54 -0500

    working

---
+++ src/components/Gallery.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import GalleryItem from './GalleryItem'
+
+function Gallery(props:any){
+
+    const display = props.data.map((item: any, index:any) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
+    })
+
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery
